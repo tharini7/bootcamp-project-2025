cfa9443837d645d1724bc2668d721e487c17e888
tharini7 <seektharini@gmail.com>
2025-12-11 23:15:37 -0800

    tst

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,11 +1,11 @@
 import BlogPreview from '@/components/blogPreview';
 import connectDB from "@/database/db";
 import BlogModel from "@/database/blogSchema";
-export const dynamic = "force-dynamic";
 
 export const metadata = { title: "Tharini - Blog" };
 
 export default async function Blog() {
+  try {
   await connectDB();
   const blogs = await BlogModel.find().sort({date: -1}).lean();
 
@@ -29,4 +29,13 @@ export default async function Blog() {
       <div id="blog-container"></div>
     </div>
   );
+   } catch (err: any) {
+    // If something goes wrong, show the error
+    return (
+      <div>
+        <h1>Error loading blogs</h1>
+        <p>{String(err)}</p>
+      </div>
+    );
+  }
 }
